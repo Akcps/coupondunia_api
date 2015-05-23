9d1453b460d7541a13782273ac2705fee22a669f
Ajit Kumar <ajit.kumar@olacabs.com>
2015-05-23 14:58:40 +0530

    added caching

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, request, jsonify
 import requests
 import os
 from coupondunia import search_page
+from werkzeug.contrib.cache import SimpleCache
+cache = SimpleCache()
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 app.requests_session = requests.Session()
@@ -14,12 +16,21 @@ def search():
         search_string = request.args.get('search')
         if not search_string:
             return jsonify({'status': False})
-        data = search_page(search_string)
+        val = cache.get(search_string)
+        if val is None:
+            print "calling api"
+            data = search_page(search_string)
+            print data
+            cache.set(search_string, str(data))
+        else:
+            print "using cache"
+            data = eval(val)
         return_data = {}
         return_data['status'] = True
         return_data['data'] = data
         return jsonify(return_data)
     except Exception, excpt:
+        print excpt
         return jsonify({'status': False})
 
 if __name__ == '__main__':
